8b141d942e59628829203e01bd942acecc914626
AntonioMesquit <140577539+AntonioMesquit@users.noreply.github.com>
2023-08-27 18:09:01 -0300

    resolve bugs

--- js/script.js
+++ js/script.js
@@ -5,45 +5,44 @@ const daynight = document.querySelector(".day-night");
 const noite = document.querySelector(".s");
 const body = document.querySelector("body")
 
-daynight.addEventListener('click' , () => {
+daynight.addEventListener('click', () => {
     daynight.classList.toggle("active")
     body.classList.toggle("active")
-     
-    
+
+
 })
 
 
-const tempo = setInterval(function time(){
+const tempo = setInterval(function time() {
     let horario = new Date();
     let hora = horario.getHours();
     let minuto = horario.getMinutes();
     let segundo = horario.getSeconds();
     let foto = document.querySelector(".z")
 
-    if(hora < 10){
+    if (hora < 10) {
         hora = '0' + hora
     }
 
-    if(minuto < 10){
+    if (minuto < 10) {
         minuto = '0' + minuto
     }
-    if(segundo < 10){
+    if (segundo < 10) {
         segundo = '0' + segundo
     }
+    if (hora >= 0 && hora < 6) {
+        foto.src = 'img/madrugada.png';
+    }
+    else if (hora >= 6 && hora < 12) {
+        foto.src = 'img/dia.png';
+    }
+    else if (hora >= 12 && hora < 18) {
+        foto.src = 'img/manha.png';
+    }
+    else if (hora >= 18 && hora < 24) {
+        foto.src = 'img/noite.png';
+    }
 
-    if(hora >= 0 && hora <= 6){
-        foto.src = 'img/madrugada.png'
-        }
-        else if(hora >= 6 && hora <= 12){
-            foto.src = 'img/dia.png'
-            }
-            else if(hora >= 12 && hora <= 18){
-                foto.src = 'img/manha.png'
-                }
-                else if(hora >= 18 && hora <= 0){
-                    foto.src = 'img/noite.png'
-                    }
-    
 
     horas.textContent = hora + ':'
     minutos.textContent = minuto + ':'
